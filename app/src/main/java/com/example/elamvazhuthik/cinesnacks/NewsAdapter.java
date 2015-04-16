ff2eddf962b37a0f588f4de41a4f68c9e48ab676
ElamvazhuthiK <vazhuthik@gmail.com>
2015-04-16 11:27:52 +0530

    newsfiles added

---
+++ app/src/main/java/com/example/elamvazhuthik/cinesnacks/NewsAdapter.java
@@ -0,0 +1,33 @@
+package com.example.elamvazhuthik.cinesnacks;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ElamvazhuthiK on 16/04/15.
+ */
+public class NewsAdapter extends ArrayAdapter<NewsModel.NewsItem> {
+    public NewsAdapter(Context context, ArrayList<NewsModel.NewsItem> items) {
+        super(context,R.layout.newsrow_layout, items);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater theInflater = LayoutInflater.from(getContext());
+        View theRow = theInflater.inflate(R.layout.newsrow_layout, parent, false);
+        NewsModel.NewsItem newsItem = getItem(position);
+        TextView title = (TextView)theRow.findViewById(R.id.textView1);
+        TextView description = (TextView)theRow.findViewById(R.id.textView2);
+
+        title.setText(newsItem.strTitle);
+        description.setText(newsItem.strDescription);
+        return theRow;
+    }
+}
---
+++ app/src/main/java/com/example/elamvazhuthik/cinesnacks/NewsModel.java
@@ -0,0 +1,32 @@
+package com.example.elamvazhuthik.cinesnacks;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by ElamvazhuthiK on 15/04/15.
+ */
+public class NewsModel extends Object {
+    public class NewsItem {
+
+        String strTitle = new String();
+        String strTitleIconLink = new String();
+        String strTime = new String();
+        String strDescription = new String();
+        NewsItem()
+        {
+
+        }
+    }
+    ArrayList<NewsItem>  newsAryList = new ArrayList<NewsItem>();
+    NewsModel ()
+    {
+        for(int i = 0; i<5; i++)
+        {
+            NewsItem newsItem = new NewsItem();
+            newsItem.strTitle = "Title " + (i + 1);
+            newsItem.strDescription = "Description " + (i + 1);
+            newsAryList.add(newsItem);
+        }
+    }
+}
